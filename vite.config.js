f0b77d4482c88e4047d7e8cd606b5b49f0f63325
wata57 <felipe.watai@gmail.com>
2024-12-08 21:52:09 -0300

    update pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "AnestCheck",
+        short_name: "AnestCheck",
+        description: "",
+        display: "standalone",
+        start_url: "/",
+        theme_color: "#3b82f6",
+        background_color: "#3b82f6",
+        icons: [
+          {
+            src: "web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
